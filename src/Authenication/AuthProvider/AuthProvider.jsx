d08d8b9c3e5653502b952e0fbe8282fc47cceb4a
FARUK2059 <omarfaruk34552@gmail.com>
2024-06-01 05:59:59 +0600

    firebase deploy successful

---
+++ src/Authenication/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,105 @@
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase.config";
+
+
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
+
+// Social Auth Provider
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    // const axiosPublic = useAxiosPublic();
+
+
+    // Creat a New user with Password then Registation
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // Update User Profile
+    const updateUserProfile = (name, photoURL) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photoURL
+        })
+    }
+
+    //  Password and Email Login
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // Google Login
+    const googleLogIn = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    // Logout section
+    const logOut = () => {
+        setLoading(false);
+        return signOut(auth);
+    }
+
+    // UseState changed
+    // useEffect(() => {
+    //     const unsubscribe = onAuthStateChanged(auth, currentUser => {
+    //         setUser(currentUser);
+    //         if (currentUser) {
+    //             // get token and store client
+    //             const userInfo = { email: currentUser.email };
+    //             axiosPublic.post('/jwt', userInfo)
+    //                 .then(res => {
+    //                     if (res.data.token) {
+    //                         localStorage.setItem('access-token', res.data.token);
+    //                     }
+    //                 })
+    //         }
+    //         else {
+    //             // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+    //             localStorage.removeItem('access-token');
+    //         }
+    //         setLoading(false);
+    //     });
+    //     return () => {
+    //         return unsubscribe();
+    //     }
+    // }, [axiosPublic])
+
+    useEffect(() => {
+        const unSubcribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user in th e state changed', currentUser)
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unSubcribe();
+        }
+    }, [])
+
+    const userInfo = {
+        user,
+        createUser,
+        loading,
+        signInUser,
+        googleLogIn,
+        updateUserProfile,
+        logOut,
+
+    }
+
+    return (
+        <AuthContext.Provider value={userInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
