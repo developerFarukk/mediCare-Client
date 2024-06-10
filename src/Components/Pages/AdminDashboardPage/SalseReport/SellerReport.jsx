f6e09fde796235e66e46af1d74f8af8a968890f0
FARUK2059 <omarfaruk34552@gmail.com>
2024-06-10 23:21:16 +0600

    selse report page date wise filter add complet

--- src/Components/Pages/AdminDashboardPage/SalseReport/SellerReport.jsx
+++ src/Components/Pages/AdminDashboardPage/SalseReport/SellerReport.jsx
@@ -1,25 +1,20 @@
 
-
-
-import React, { useState, useEffect } from 'react';
-import { useTable, usePagination } from 'react-table';
+import React, { useState, useEffect, useRef } from 'react';
 import { saveAs } from 'file-saver';
 import { utils, write } from 'xlsx';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-import { Document, Packer, Paragraph, Table, TableCell, TableRow, WidthType } from 'docx';
 import useAxiosSecure from '../../../../Hooks/AxiosSecure/useAxiosSecure';
-import html2canvas from 'html2canvas-pro';
-
-import { useRef } from 'react';
 import { useDownloadExcel } from 'react-export-table-to-excel';
 import { Helmet } from 'react-helmet-async';
-
+import html2canvas from 'html2canvas-pro';
 
 const SalesReport = () => {
     const axiosSecure = useAxiosSecure();
     const [salesData, setSalesData] = useState([]);
-    const [pageSize, setPageSizeState] = useState(10);
+    const [filteredSalesData, setFilteredSalesData] = useState([]);
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
     const tableRef = useRef(null);
 
@@ -27,61 +22,58 @@ const SalesReport = () => {
         fetchSalesData();
     }, []);
 
-    console.log(salesData);
+    useEffect(() => {
+        filterDataByDate();
+    }, [startDate, endDate, salesData]);
 
     const fetchSalesData = async () => {
         try {
-            const response = await axiosSecure.get('/payment'); // Adjust the endpoint as needed
+            const response = await axiosSecure.get('/payment');
             setSalesData(response.data);
         } catch (error) {
             console.error('Error fetching sales data:', error);
         }
     };
 
+    const filterDataByDate = () => {
+        const filteredData = salesData.filter(row => {
+            const rowDate = new Date(row.date);
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+
+            if (startDate && endDate) {
+                return rowDate >= start && rowDate <= end;
+            } else if (startDate) {
+                return rowDate >= start;
+            } else if (endDate) {
+                return rowDate <= end;
+            } else {
+                return true;
+            }
+        });
+
+        setFilteredSalesData(filteredData);
+    };
 
     const columns = React.useMemo(
         () => [
             { Header: 'SL No', accessor: 'SL No' },
-            { Header: 'Buyer Email', accessor: 'buyerEmail' },
+            { Header: 'Buyer Email', accessor: 'email' },
             { Header: 'Seller Email', accessor: 'sellerEmail' },
             { Header: 'Medicine Name', accessor: 'menuItemName' },
-            { Header: 'Medicine ID', accessor: 'menuItemIds' },
             { Header: 'Medicine Price', accessor: 'idPrice' },
             { Header: 'Date', accessor: 'date' },
             { Header: 'Total Price', accessor: 'price' },
-
         ],
         []
     );
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        page,
-        setPageSize,
-        state: { pageSize: statePageSize },
-    } = useTable(
-        {
-            columns,
-            data: salesData,
-            initialState: { pageIndex: 0, pageSize },
-        },
-        usePagination
-    );
-
-    const handleSetPageSize = (size) => {
-        setPageSize(size);
-        setPageSizeState(size);
-    };
-
-
     const exportCSV = () => {
-        const csvData = salesData.map((row, index) => ({
+        const csvData = filteredSalesData.map((row, index) => ({
             'SL NO': index + 1,
             'Buyer Email': row.email,
             'Seller Email': row.sellerEmail.join(', '),
             'Medicine Name': row.menuItemName.join(', '),
-            'Medicine ID': row.menuItemIds.join(', '),
             'Medicine Price': row.idPrice.join(', '),
             'Date': new Date(row.date).toLocaleDateString(),
             'Total Price': row.price
@@ -91,12 +83,11 @@ const SalesReport = () => {
         saveAs(new Blob([csvBuffer], { type: 'text/csv' }), 'sales_report.csv');
     };
 
-    // Export Excel file
     const { onDownload } = useDownloadExcel({
         currentTableRef: tableRef.current,
-        filename: 'Users table',
-        sheet: 'Users'
-    })
+        filename: 'Sales Report',
+        sheet: 'Sales'
+    });
 
     const generatePDF = async () => {
         const element = document.getElementById('content-id');
@@ -127,153 +118,84 @@ const SalesReport = () => {
         pdf.save('invoice.pdf');
     };
 
-    const exportDOCX = () => {
-        const doc = new Document();
-
-        // Add a title to the document
-        doc.addSection({
-            properties: {},
-            children: [
-                new Paragraph({
-                    text: "Sales Report",
-                    // heading: HeadingLevel.TITLE,
-                    // alignment: AlignmentType.CENTER,
-                }),
-            ],
-        });
-
-        // Create table rows for headers and data
-        const tableRows = [
-            new TableRow({
-                children: [
-                    new TableCell({ children: [new Paragraph('SL No')] }),
-                    new TableCell({ children: [new Paragraph('Buyer Email')] }),
-                    new TableCell({ children: [new Paragraph('Seller Email')] }),
-                    new TableCell({ children: [new Paragraph('Medicine Name')] }),
-                    new TableCell({ children: [new Paragraph('Medicine ID')] }),
-                    new TableCell({ children: [new Paragraph('Medicine Price')] }),
-                    new TableCell({ children: [new Paragraph('Date')] }),
-                    new TableCell({ children: [new Paragraph('Total Price')] }),
-                ],
-            }),
-        ];
-
-        salesData.forEach((row, index) => {
-            const sellerEmail = row.sellerEmail ? row.sellerEmail.join(', ') : '';
-            const menuItemName = row.menuItemName ? row.menuItemName.join(', ') : '';
-            const menuItemIds = row.menuItemIds ? row.menuItemIds.join(', ') : '';
-            const idPrice = row.idPrice ? row.idPrice.join(', ') : '';
-
-            tableRows.push(
-                new TableRow({
-                    children: [
-                        new TableCell({ children: [new Paragraph((index + 1).toString())] }),
-                        new TableCell({ children: [new Paragraph(row.email)] }),
-                        new TableCell({ children: [new Paragraph(sellerEmail)] }),
-                        new TableCell({ children: [new Paragraph(menuItemName)] }),
-                        new TableCell({ children: [new Paragraph(menuItemIds)] }),
-                        new TableCell({ children: [new Paragraph(idPrice)] }),
-                        new TableCell({ children: [new Paragraph(new Date(row.date).toLocaleDateString())] }),
-                        new TableCell({ children: [new Paragraph(row.price.toString())] }),
-                    ],
-                })
-            );
-        });
-
-        const table = new Table({
-            rows: tableRows,
-            width: {
-                size: 100,
-                type: WidthType.PERCENTAGE,
-            },
-        });
-
-        doc.addSection({
-            children: [table],
-        });
-
-        Packer.toBlob(doc).then(blob => {
-            saveAs(blob, 'sales_report.docx');
-        });
-    };
-
-
-    console.log(page);
-
     return (
         <div>
-
-            {/* Dynamic Title section */}
             <div>
                 <Helmet>
-                    <title> Seller Report | MediCare</title>
+                    <title>Seller Report | MediCare</title>
                 </Helmet>
             </div>
 
-
-
-            {/* Titel section */}
             <div className="text-center">
-                <h2 className="lg:text-4xl text-2xl font-bold p-4"> Manage Seles Report </h2>
+                <h2 className="lg:text-4xl text-2xl font-bold p-4">Manage Sales Report</h2>
             </div>
 
-            {/* Filter section */}
-            <div className='justify-end flex p-2'>
-                <select
-                    className='rounded-lg'
-                    value={statePageSize}
-                    onChange={e => handleSetPageSize(Number(e.target.value))}
-                >
-                    {[10, 20, 30, 40, 50, 100].map(pageSize => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
+            {/* Date Range Filter Section */}
+            <div className="flex justify-center space-x-4 p-4">
+                <div>
+                    <label htmlFor="startDate" className="block text-sm font-medium text-gray-700">
+                        Start Date
+                    </label>
+                    <input
+                        type="date"
+                        id="startDate"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        value={startDate}
+                        onChange={e => setStartDate(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="endDate" className="block text-sm font-medium text-gray-700">
+                        End Date
+                    </label>
+                    <input
+                        type="date"
+                        id="endDate"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        value={endDate}
+                        onChange={e => setEndDate(e.target.value)}
+                    />
+                </div>
             </div>
 
-            {/* Table Section */}
-
-
-            {/* Table section */}
             <div ref={tableRef} id="content-id" className="overflow-x-auto">
-                <table {...getTableProps()} className="min-w-full divide-y-2 divide-gray-200  text-sm dark:divide-gray-700 dark:bg-gray-900 rounded-lg">
+                <table className="min-w-full divide-y-2 divide-gray-200  text-sm dark:divide-gray-700 dark:bg-gray-900 rounded-lg">
                     <thead className="bg-green-300">
                         <tr>
                             <th className="whitespace-nowrap text-start px-4 py-2 font-medium text-gray-900 dark:text-white">SL</th>
                             <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Buyer Email</th>
-                            <th className="whitespace-nowrap   px-4 text-center py-2 font-medium text-gray-900 dark:text-white">Seller Email</th>
-                            <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Medicin ID</th>
-                            <th className="whitespace-nowrap px-4 text-start py-2 font-medium text-gray-900 dark:text-white">Medicin Price</th>
-                            <th className="whitespace-nowrap px-4 text-center py-2 font-medium text-gray-900 dark:text-white">Date</th>
-                            <th className="whitespace-nowrap px-4 text-center py-2 font-medium text-gray-900 dark:text-white">Total Price</th>
+                            <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Seller Email</th>
+                            <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Medicine Name</th>
+                            <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Medicine Price</th>
+                            <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Date</th>
+                            <th className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900 dark:text-white">Total Price</th>
                         </tr>
                     </thead>
-                    <tbody {...getTableBodyProps()} className="divide-y divide-gray-200 p-1 dark:divide-gray-700">
-                        {page.map((medi, index) => (
+                    <tbody className="divide-y divide-gray-200 p-1 dark:divide-gray-700">
+                        {filteredSalesData.map((medi, index) => (
                             <tr className="hover:bg-blue-200" key={medi._id}>
                                 <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">{index + 1}</td>
-                                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">{medi.original.email}</td>
+                                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">{medi.email}</td>
                                 <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-                                    {medi.original.sellerEmail?.map((seller, i) => (
+                                    {medi.sellerEmail?.map((seller, i) => (
                                         <div key={i}>{seller}</div>
                                     ))}
                                 </td>
                                 <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-                                    {medi.original.menuItemIds?.map((id, i) => (
-                                        <div key={i}>{id}</div>
+                                    {medi.menuItemName?.map((name, i) => (
+                                        <div key={i}>{name}</div>
                                     ))}
                                 </td>
                                 <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-                                    {medi.original.idPrice?.map((price, i) => (
+                                    {medi.idPrice?.map((price, i) => (
                                         <div key={i} className="flex justify-end">${price}</div>
                                     ))}
                                 </td>
                                 <td className="items-center gap-2 justify-center p-1 text-end">
-                                    <p>{new Date(medi.original.date).toLocaleDateString()}</p>
+                                    <p>{new Date(medi.date).toLocaleDateString()}</p>
                                 </td>
                                 <td className="items-center gap-2 justify-center p-3 text-end">
-                                    <p>${medi.original.price}</p>
+                                    <p>${medi.price}</p>
                                 </td>
                             </tr>
                         ))}
@@ -281,12 +203,10 @@ const SalesReport = () => {
                 </table>
             </div>
 
-
-            <div className='flex gap-4 justify-center p-2 '>
+            <div className='flex gap-4 justify-center p-2'>
                 <button className='btn btn-primary' onClick={exportCSV}>Export CSV</button>
                 <button className='btn btn-primary' onClick={onDownload}>Export Excel</button>
                 <button className='btn btn-primary' onClick={generatePDF}>Export PDF</button>
-                <button className='btn btn-primary' onClick={exportDOCX}>Export DOCX</button>
             </div>
         </div>
     );
